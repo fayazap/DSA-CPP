e00669abfa3670f7ac0f8515f25cef4f20015315
fayazap <84265684+fayazap@users.noreply.github.com>
2022-08-28 14:06:40 +0530

    Create Insertion.c

---
+++ Sorting Algos/Insertion.c
@@ -0,0 +1,15 @@
+void insertionSort(int arr[], int n)
+    {
+        for(int i=0; i<=n; i++)
+        {
+            for(int j = 1; j<i; j++)
+            {
+                if(arr[j]<arr[j-1])
+                {
+                    int temp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
